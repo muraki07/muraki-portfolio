cd8ee8a61b3cac001988aea30d64c790335fa7ff
muraki07 <r.muraki42@gmail.com>
2024-06-21 09:18:17 +0900

    Add files via upload

---
+++ user/inc/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>-聴こえないひとのための一人旅情報-Silent Adventure</title>
+  <meta name="description" content="『Silent Adventure』は、聴覚障害者の一人旅のための情報提供をしているWebサイトです。一人旅の不安払拭に繋がるような情報や、聴覚障害者の体験談をお伝えしています。" />
+  <meta name="author" content="silentadventure" />
+  <link rel="icon" href="../img/favicon.png" />
+  <link rel="stylesheet" href="../css/sanitize.css" />
+  <link rel="stylesheet" href="../css/style.css" />
+  <script src="../js/jquery-3.7.1.min.js"></script>
+  <script src="../js/jquery.easing.1.3.js"></script>
+</head>
+
+<body>
+  <header id="header">
+    <div class="header-logo">
+      <a href="index.php"><img src="../img/logo.png" alt="silentadventure" /></a>
+    </div>
+    <nav class="nav">
+      <ul class="nav-list">
+        <li>
+          <a href="index.php">
+            <p class="nav-ja">トップ</p>
+            <p class="nav-en">TOP</p>
+          </a>
+        </li>
+        <li>
+          <a href="contents.php">
+            <p class="nav-ja">記事一覧</p>
+            <p class="nav-en">CONTENTS</p>
+          </a>
+        </li>
+        <li>
+          <a href="index.php#about">
+            <p class="nav-ja">私たちについて</p>
+            <p class="nav-en">ABOUT US</p>
+          </a>
+        </li>
+        <li>
+          <a href="index.php#contact">
+            <p class="nav-ja">お問い合わせ</p>
+            <p class="nav-en">CONTACT</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <div class="hamburger-menu"><span></span><span></span><span></span></div>
+  </header>
