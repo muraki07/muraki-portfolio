2caf27ff6aeccaf6a6ff6f78e26e578162b71478
muraki07 <r.muraki42@gmail.com>
2024-06-21 09:21:33 +0900

    Update footer.php

--- company/inc/footer.php
+++ company/inc/footer.php
@@ -1,1 +1,38 @@
+<footer id="footer">
+  <button id="js-page-top" class="page-top">
+    <a href="#" class="page-top-arrow"></a>
+  </button>
+  <nav class="footer-nav">
+    <ul class="footer-nav-list">
+      <li>
+        <a href="article.php">
+          <p class="nav-ja">記事一覧</p>
+          <p class="nav-en">ARTICLE</p>
+        </a>
+      </li>
+      <li>
+        <a href="add-form.php">
+          <p class="nav-ja">記事追加</p>
+          <p class="nav-en">ADD ARTICLE</p>
+        </a>
+      </li>
+      <li>
+        <a href="logout.php">
+          <p class="nav-ja">ログアウト</p>
+          <p class="nav-en">LOGOUT</p>
+        </a>
+      </li>
+    </ul>
+  </nav>
 
+  <div class="footer-wrapper">
+    <div class="footer-logo">
+      <a href="article.php"><img src="../img/logo.png" alt="silentadventure" /></a>
+    </div>
+    <p class="copyright">&copy; Silent Adventure</p>
+  </div>
+</footer>
+
+<script src="../js/pagetop.js"></script>
+<script src="../js/preview.js"></script>
+<script src="../js/delete-dialog.js"></script>
