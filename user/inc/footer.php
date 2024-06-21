45755457ea95de8d9f2850d873810ec2712b8137
muraki07 <r.muraki42@gmail.com>
2024-06-21 09:17:45 +0900

    Update footer.php

--- user/inc/footer.php
+++ user/inc/footer.php
@@ -1,1 +1,48 @@
+<footer id="footer">
+  <button id="js-page-top" class="page-top">
+    <a href="#" class="page-top-arrow"></a>
+  </button>
+  <nav class="footer-nav">
+    <ul class="footer-nav-list">
+      <li>
+        <a href="index.php">
+          <p class="nav-ja">トップ</p>
+          <p class="nav-en">HOME</p>
+        </a>
+      </li>
+      <li>
+        <a href="contents.php">
+          <p class="nav-ja">記事一覧</p>
+          <p class="nav-en">CONTENTS</p>
+        </a>
+      </li>
+      <li>
+        <a href="index.php#about">
+          <p class="nav-ja">私たちについて</p>
+          <p class="nav-en">ABOUT US</p>
+        </a>
+      </li>
+      <li>
+        <a href="index.php#contact">
+          <p class="nav-ja">お問い合わせ</p>
+          <p class="nav-en">CONTACT</p>
+        </a>
+      </li>
+    </ul>
+  </nav>
 
+  <div class="footer-wrapper">
+    <div class="footer-logo">
+      <a href="index.php"><img src="../img/logo.png" alt="silentadventure" /></a>
+    </div>
+    <p class="copyright">&copy; Silent Adventure</p>
+  </div>
+</footer>
+
+<script src="../js/hamburgermenu.js"></script>
+<script src="../js/textanimetion.js"></script>
+<script src="../js/contact-load.js"></script>
+<script src="../js/pagetop.js"></script>
+</body>
+
+</html>
