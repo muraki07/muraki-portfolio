1891cca2ef3c33d3bfbd0eec816bba114ff41ad1
muraki07 <r.muraki42@gmail.com>
2024-06-21 09:12:51 +0900

    Update add-count.js

--- js/add-count.js
+++ js/add-count.js
@@ -1,1 +1,13 @@
+// 記事追加フォーム文字数カウント処理
+const addTextarea = document.getElementById("add-content");
+const addCounters = document.querySelectorAll(".count");
 
+addTextarea.addEventListener("input", function () {
+  const maxLength = parseInt(addTextarea.getAttribute("maxlength"), 10);
+  const currentLength = addTextarea.value.length;
+  const remaining = maxLength - currentLength;
+
+  addCounters.forEach(function (counter) {
+    counter.textContent = remaining;
+  });
+});
